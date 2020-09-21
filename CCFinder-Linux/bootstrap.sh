55264610967f60e184e4b66d028c5c66209b32e2
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-21 19:36:33 -0400

    Update bootstrap.sh

--- CCFinder-Linux/bootstrap.sh
+++ CCFinder-Linux/bootstrap.sh
@@ -1,9 +1,7 @@
 
 sudo apt update; 
 sudo apt-get install boost. build-essential git sudo g++ zlibc make gdb -y;
-cd ~;
 git clone https://github.com/microsoft/ProcDump-for-Linux.git;
-cd CCFinder-Linux;
 g++ ccfinder-linux.cpp -o ccfinder -lboost_filesystem -lboost_system -lboost_iostreams -lboost_regex;
 sudo cp ccfinder /usr/sbin;
 cd ..;

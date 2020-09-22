409d951e1ac8eb2bfd4dd460a5458dd366a19911
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-22 05:31:27 -0400

    code optimization

--- CCFinder-Linux/ccfinder-linux.cpp
+++ CCFinder-Linux/ccfinder-linux.cpp
@@ -62,71 +62,71 @@ int main(int argc, char* argv[])
 				while (getline(myfile, line))
 				{
 				if (strstr(entry.c_str(),t.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t11(".qcow");
 				if (strstr(entry.c_str(),t11.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t1(".iso");
 				if (strstr(entry.c_str(),t1.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t2(".jar");
 				if (strstr(entry.c_str(),t2.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t3(".qed");
 				if (strstr(entry.c_str(),t3.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t4(".img");
 				if (strstr(entry.c_str(),t4.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t5(".swp");
 				if (strstr(entry.c_str(),t5.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t6(".raw");
 				if (strstr(entry.c_str(),t6.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t7(".mp4");
 				if (strstr(entry.c_str(),t7.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t8(".mov");
 				if (strstr(entry.c_str(),t8.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t9(".avi");
 				if (strstr(entry.c_str(),t9.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t10(".mp3");
 				if (strstr(entry.c_str(),t10.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t111(".jpg");
 				if (strstr(entry.c_str(),t111.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t12(".png");
 				if (strstr(entry.c_str(),t12.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t13(".gif");
 				if (strstr(entry.c_str(),t13.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t14(".so");
 				if (strstr(entry.c_str(),t14.c_str())) {
-				return 0;
+				goto loop;
 				}
 				std::string t15(".md");
 				if (strstr(entry.c_str(),t15.c_str())) {
-				return 0;
+				goto loop;
 				}
 				if (!myfile) {
 				cout << " Failed to open " << entry << endl;
@@ -138,6 +138,7 @@ int main(int argc, char* argv[])
 						}
 					}
 				}
+				loop:;
 			}
 			catch (const std::exception & ex)
 			{

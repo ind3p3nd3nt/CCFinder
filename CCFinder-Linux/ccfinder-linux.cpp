79f3c05f01a8709f9e997bcc6c2a6ef3ed79bb3d
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-22 01:22:49 -0400

    Update ccfinder-linux.cpp

--- CCFinder-Linux/ccfinder-linux.cpp
+++ CCFinder-Linux/ccfinder-linux.cpp
@@ -35,44 +35,6 @@ std::string ConvertToStringUsingBoost(ValueType const& v)
 }
 
 
-
-void PrintProcessNameAndID(DWORD processID)
-{
-
-	TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
-
-	// Get a handle to the process.
-
-	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION |
-		PROCESS_VM_READ,
-		FALSE, processID);
-
-	// Get the process name.
-
-	if (NULL != hProcess)
-	{
-		HMODULE hMod;
-		DWORD cbNeeded;
-
-		if (EnumProcessModules(hProcess, &hMod, sizeof(hMod),
-			&cbNeeded))
-		{
-			GetModuleBaseName(hProcess, hMod, szProcessName,
-				sizeof(szProcessName) / sizeof(TCHAR));
-		}
-	}
-
-	// Print the process name and identifier.
-
-	_tprintf(TEXT("%s  (PID: %u)\n"), szProcessName, processID);
-
-
-
-    string cmd = "/usr/bin/procdump -accepteula -mp -a -r 5 " + ConvertToStringUsingBoost(processID);
-	cout << cmd.c_str();
-	system(cmd.c_str());
-}
-
 bool checkLuhn(const string& cardNo)
 {
 	int nDigits = cardNo.length();
@@ -167,4 +129,85 @@ int main()
 					}
 				}
 		}
+}
+#include <boost/regex.hpp> 
+#include <iostream> 
+#include <boost/filesystem.hpp>
+#include <vector>
+#include <string>
+#include "header.h"
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <fstream>
+
+using namespace std;
+using namespace boost::filesystem;
+namespace fs = boost::filesystem;
+namespace io = boost::iostreams;
+
+
+bool checkLuhn(const string& cardNo)
+{
+	int nDigits = cardNo.length();
+
+	int nSum = 0, isSecond = false;
+	for (int i = nDigits - 1; i >= 0; i--) {
+
+		int d = cardNo[i] - '0';
+
+		if (isSecond == true)
+			d = d * 2;
+		nSum += d / 10;
+		nSum += d % 10;
+
+		isSecond = !isSecond;
+	}
+	return (nSum % 10 == 0);
+}
+
+int main(int argc, char* argv[])
+{
+	cout << "CC Finder by independent 3.3 https://GitHub.com/independentcod All rights reserved" << std::endl;
+	cout << "Please wait a few hours this will take a while..." << std::endl;
+	cout << "---" << std::endl;
+	std::ofstream fout("CCFinder.log", ios::app);
+	fout << "CC Finder by independent 3.3 https://GitHub.com/independentcod All rights reserved" << std::endl;
+	std::ifstream ifile("/usr/bin/procdump");
+	if ((bool)ifile) {
+		system("for i in $(ps -axo pid); do sudo /usr/bin/procdump -p $i; done");
+			}
+	for (const auto& x : fs::recursive_directory_iterator(argv[1])) {
+		if (boost::filesystem::is_regular_file(x.path())) {
+			boost::filesystem::absolute(x.path().filename());
+			fs::path entry = x;
+			std::string line;
+			std::cout << entry << std::endl;
+			try
+			{
+
+				boost::iostreams::stream_buffer<boost::iostreams::file_descriptor_source> bis(entry);
+				std::istream myfile(&bis);
+				boost::regex expr1("\\b[3-5]\\d{15,16}\\b");
+				boost::smatch what1;
+				if (!myfile) {
+					cout << " Failed to open " << entry << endl;
+				}
+				while (getline(myfile, line))
+				{
+					if (boost::regex_search(line, what1, expr1))
+					{
+						if (checkLuhn(what1.str())) {
+							fout << line << std::endl;
+						}
+					}
+
+				}
+			}
+			catch (const std::exception & ex)
+			{
+
+				std::cout << entry << " " << std::endl;
+			}
+		}
+	}
 }

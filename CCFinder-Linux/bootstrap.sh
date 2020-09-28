7f9876ac52971b57e82a03eec266699cd6afc4fb
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-28 07:03:41 -0400

    Update bootstrap.sh

--- CCFinder-Linux/bootstrap.sh
+++ CCFinder-Linux/bootstrap.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 sudo apt update; 
 sudo apt-get install boost. build-essential git sudo g++ -y;
-#git clone https://github.com/microsoft/ProcDump-for-Linux.git;
+if [ ! -f "/usr/bin/procdump" ]; then
+wget -O procdump.deb https://github.com/microsoft/ProcDump-for-Linux/releases/download/1.1.1/procdump_1.1.1-220_amd64.deb
+sudo dpkg -i procdump.deb
+sudo apt -f install -y
+rm -rf procdump.deb;
+fi
 g++ ccfinder-linux.cpp -o ccfinder -lboost_filesystem -lboost_system -lboost_iostreams -lboost_regex;
 sudo chmod +x ccfinder;
 sudo cp ccfinder /usr/sbin/ccfinder -r;
-#cd ProcDump-for-Linux;
-#make -j8 && sudo make install;
-#cd ..;

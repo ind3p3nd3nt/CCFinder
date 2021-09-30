895ebca40946ad15b1c599a306e64e9c779f70b2
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-09-30 00:06:48 -0400

    Update bootstrap.sh

--- CCFinder-Linux/bootstrap.sh
+++ CCFinder-Linux/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -f /usr/bin/apt ]; then apt-get update && apt-get install libboost-dev build-essential git g++ -y; else yum install c++ boost-devel; fi;
+if [ -f /usr/bin/apt ]; then apt-get update && apt-get install libboost-dev build-essential git g++ -y; else yum groupinstall 'Development Tools' -y && yum install boost-devel; fi;
 g++ ccfinder-linux.cpp -o ccfinder -lboost_filesystem -lboost_system -lboost_iostreams -lboost_regex;
 chmod +x ccfinder;
 cp ccfinder /usr/sbin/ccfinder -r;

2b57fe43272466e4fbdabea82dad36a1cd36a3e7
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2021-10-26 09:27:41 -0400

    Update bootstrap.sh

--- CCFinder-Linux/bootstrap.sh
+++ CCFinder-Linux/bootstrap.sh
@@ -2,6 +2,7 @@
 if [ -f /usr/bin/apt ]; then apt-get update && apt-get install libboost-all-dev build-essential -y; 
 else
 yum groupinstall 'Development Tools' -y 
+if [ ! -f boost_1_77_0.tar.gz ]; then
 wget https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz
 tar xvf boost_1_77_0.tar.gz
 cd boost_1_77_0
@@ -9,6 +10,7 @@ cd boost_1_77_0
 ./b2 install --prefix='/opt/boost' --with=all -j8
 cp -r /opt/boost/* /usr/
 cd ..;
+fi;
 fi
 g++ ccfinder-linux.cpp -o ccfinder -lboost_filesystem -lboost_system -lboost_iostreams -lboost_regex --std=c++11;
 chmod +x ccfinder;
